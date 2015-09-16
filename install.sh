650d20a53917007641805ffe3926f32075bb67de
Jonathan Tsai <jontsai@jonathantsai.com>
2015-09-16 19:47:04 +0000

    update submodule pointers to jontsai, add editorconfig-emacs

--- install.sh
+++ install.sh
@@ -4,15 +4,20 @@ INSTALL_DIR=~/.site-lisp
 
 mkdir -p ${INSTALL_DIR}
 
-cp highlight-current-line.elc ${INSTALL_DIR}/
 cp jontsai.emacs.elc ~/.emacs.elc
 
 # language modes
 LANGUAGE_MODES="csharp php"
 for lang in $LANGUAGE_MODES
 do
-    cmd="cp ${lang}-mode/${lang}-mode.elc ${INSTALL_DIR}/"
-    echo $cmd
-    $cmd
+    cp -v ${lang}-mode/${lang}-mode.elc ${INSTALL_DIR}/
 done
+
+# miscellaneous files
+EL_FILES="highlight-current-line editorconfig-emacs/editorconfig"
+for elfile in $EL_FILES
+do
+    cp -v ${elfile}.elc  ${INSTALL_DIR}/
+done
+
 
