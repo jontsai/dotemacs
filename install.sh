127ae6cdf050c86fcad0a15e99bbb24294788519
Jonathan Tsai (jontsai) <jontsai@iterable.com>
2016-03-22 11:43:04 -0700

    add yaml-mode

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p ${INSTALL_DIR}
 cp jontsai.emacs.elc ~/.emacs.elc
 
 # language modes
-LANGUAGE_MODES="csharp php"
+LANGUAGE_MODES="csharp php yaml"
 for lang in $LANGUAGE_MODES
 do
     cp -v ${lang}-mode/${lang}-mode.elc ${INSTALL_DIR}/
