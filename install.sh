6c3d99cefc57392299560512d174f1460f0d2075
Jonathan Tsai <hello@jontsai.com>
2020-05-03 23:55:28 +0000

    update language mode submodules

--- install.sh
+++ install.sh
@@ -13,12 +13,13 @@ for lang in $LANGUAGE_MODES
 do
     if [ $lang == 'scala' ]
     then
-        cp -v scala-mode2/*.el* ${INSTALL_DIR}/
+        cp -v scala-mode/*.el* ${INSTALL_DIR}/
     else
         MODE_FILE=${lang}-mode/${lang}-mode.elc
         cp -v ${MODE_FILE} ${INSTALL_DIR}/
     fi
 done
+
 
 # Miscellaneous .elc files
 ELC_FILES="highlight-current-line editorconfig/editorconfig editorconfig/editorconfig-core editorconfig/editorconfig-core-handle editorconfig/editorconfig-fnmatch py-isort/py-isort"
